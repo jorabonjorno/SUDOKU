c699ebd76a7d0703fcfdcb00eb98f1fdb31494a9
Alevtina <a1evtina@list.ru>
2022-06-10 15:57:18 +0300

    variants

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,5 @@
 const string = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89---'
+const check = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 const rows = rowsGenerator(string)
 const columns = columnsGenerator(string)
 const squares = squaresGenerator(string)
@@ -41,7 +42,57 @@ function squaresGenerator (string) {
   return squaresArray;
   }
 
-console.log(squares)
+  function numbersRemove (array, target) { // убирает из массива от 1 до 9 найденное в целевом массиве число
+    let arrayChecked = [...check]; 
+    let initialArray = [...array]; 
+    let index;
+    for (i = 0; i < arrayChecked.length; i++) {
+      if (initialArray[i] === target) {
+      index = arrayChecked.indexOf(target);
+      arrayChecked.splice(index, 1)}
+    }
+    return arrayChecked
+  }
+
+function count () { // счетчик итоговый (проверка суммы в ряду и колонке)
+  let countRow = 0;
+  let countColumn = 0;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) {
+      if (rows[i][j] !== '-') {
+        countRow += +rows[i][j]
+      }
+  }
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) {
+      if (columns[i][j] !== '-') {
+        countColumn += +columns[i][j]
+      }
+  }
+}
+if (countRow === 45 && countColumn === 45) {
+  return true
+} else {
+  return false
+}
+}}
+
+function variants (array, index) { // массив с числами которых нет в проверяемом ряду и колонке
+  let variantsArray = []
+      if (rows[array][index] === '-') {
+        for (i = 0; i < 9; i++) {
+        variantsArray = numbersRemove (rows[array], variantsArray[i]); 
+        variantsArray = numbersRemove (columns[index], variantsArray[i]); 
+        return variantsArray
+      }
+}
+}
+
+console.log(check[1])
+console.log(rows[0][1])
+console.log(variants(0, 1))
+console.log(numbersRemove(rows[1], 5))
+
 
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
