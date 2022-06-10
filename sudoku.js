b6a51d65f1bb00a5c7f9f77ab3c7dc3334b4ac50
Alevtina <a1evtina@list.ru>
2022-06-10 13:57:22 +0300

    arrays

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,48 @@
+const string = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89---'
+const rows = rowsGenerator(string)
+const columns = columnsGenerator(string)
+const squares = squaresGenerator(string)
+function rowsGenerator (string) {
+    let rowsArray = [];
+    rowsArray.push(string.split('', 9));
+    for (i = 9; i < 81; i+=9) {
+    let rows = string.slice(i, i+9);
+    rowsArray.push(rows.split(''));
+    }
+    return rowsArray;
+}
+
+function columnsGenerator () {
+  let columnsArray = rows[0].map((_, index) => rows.map(column => column[index]));
+  return columnsArray;
+}
+
+console.log(columns)
+console.log(rows)
+
+function squaresGenerator (string) {
+  let squaresArray = []
+  let arrayNumber = 0;
+  for (i = 1; i < 81; i++) {
+      let subArray = [];        
+      squaresArray.push(subArray)    
+      for (j = i + 1; j < 81; j + 3) {
+      squaresArray[arrayNumber].push(string[j], string[j - 1], string[j - 2]);
+      squaresArray[arrayNumber].push(string[j + 7], string[j + 8], string[j + 9]);
+      squaresArray[arrayNumber].push(string[j + 16], string[j + 17], string[j + 18]);
+      arrayNumber += 1
+      break
+      }
+      if (i % 9 === 0) {
+        i += 21
+      }
+    }
+
+  return squaresArray;
+  }
+
+console.log(squares)
+
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
